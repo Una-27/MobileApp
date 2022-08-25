a0f530e60242d73d0b1120de39914d872dcdbf9a
Una-27 <76427393+Una-27@users.noreply.github.com>
2022-08-25 17:49:00 +0300

    Error correction

---
+++ Calcul/SerializableObject.cs
@@ -0,0 +1,82 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+
+namespace ModelLibrary
+{
+    public abstract class SerializableObject
+    {
+
+    }
+
+    public enum RecipeType
+    {
+        Primary = 0,
+        Secondary = 1,
+        Garnish = 2,
+        ColdSnacks = 3,
+        Drink = 4
+    }
+
+    public static class Tools 
+    {
+        public static string GetEnumName(this Enum enumValue)
+        {
+            return enumValue.GetType()
+                            .GetMember(enumValue.ToString())
+                            .First().Name;
+        }
+    }
+
+    public class Recipe : SerializableObject
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public int Weight { get; set; }
+        public int Colories { get; set; }
+        public int Proteins { get; set; }
+        public int Greases { get; set; }
+        public int Carbohydrates { get; set; }
+        public bool IsVegan { get; set; }
+        public bool IsVegetarian { get; set; }
+        public byte[] MainPicture { get; set; }
+        public int PictureCount { get; set; }
+        public RecipeType Type { get; set; }
+    }
+
+    public class DayMenu : SerializableObject
+    { 
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public String Date { get; set; }
+        public List<Recipe> BreakfastRecipes { get; set; } = new List<Recipe>();
+        public List<Recipe> LaunchRecipes { get; set; } = new List<Recipe>();
+        public List<Recipe> DinnerRecipes { get; set; } = new List<Recipe>();
+    }
+
+    public static class Serialization<T> where T : SerializableObject
+    {
+        public static List<T> ReadList(string bytes)
+        {
+            return JsonConvert.DeserializeObject<List<T>>(bytes);
+        }
+
+        public static string WriteList(List<T> recipes)
+        {
+            return JsonConvert.SerializeObject(recipes.ToArray());
+        }
+
+        public static T Read(string bytes)
+        {
+            return JsonConvert.DeserializeObject<T>(bytes);
+        }
+
+        public static string Write(T recipe)
+        {
+            return JsonConvert.SerializeObject(recipe);
+        }
+    }
+}
---
+++ Calcul/obj/Debug/90/android/src/crc64a02c20940f31da7f/MainActivity_SplashActivity.java
@@ -0,0 +1,56 @@
+package crc64a02c20940f31da7f;
+
+
+public class MainActivity_SplashActivity
+	extends android.support.v7.app.AppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;Landroid/os/PersistableBundle;)V:GetOnCreate_Landroid_os_Bundle_Landroid_os_PersistableBundle_Handler\n" +
+			"n_onResume:()V:GetOnResumeHandler\n" +
+			"";
+		mono.android.Runtime.register ("Calcul.MainActivity+SplashActivity, Calcul", MainActivity_SplashActivity.class, __md_methods);
+	}
+
+
+	public MainActivity_SplashActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity_SplashActivity.class)
+			mono.android.TypeManager.Activate ("Calcul.MainActivity+SplashActivity, Calcul", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0, android.os.PersistableBundle p1)
+	{
+		n_onCreate (p0, p1);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0, android.os.PersistableBundle p1);
+
+
+	public void onResume ()
+	{
+		n_onResume ();
+	}
+
+	private native void n_onResume ();
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
